b938a2d35ce8ce50f5de2a00a796bb4b5c498f28
stsav012 <stsav012@gmail.com>
2022-03-29 11:06:38 +0300

    To behave like an application

---
+++ __main__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        sys.stderr.write('\033[1musage:\033[0m %s file1 ...' % sys.argv[0])
+
+    else:
+        def run() -> None:
+            import sys
+            from pathlib import Path
+            from PySide6.QtWidgets import QApplication
+            from grd_reader import plot
+
+            app: QApplication = QApplication(sys.argv)
+            plot: plot.Plot = plot.Plot()
+            plot.plot(Path(sys.argv[1]))
+            plot.show()
+            app.exec()
+
+        run()
